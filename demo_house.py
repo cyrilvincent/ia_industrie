44c534ac37faac26d1db2a56e7b8e9828c960351
Cyril Vincent <contact@cyrilvincent.com>
2024-11-20 14:20:24 +0100

    Seche

--- demo_house.py
+++ demo_house.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn.linear_model as lm
 
 print(pd.__version__)
 
@@ -8,8 +9,22 @@ dataframe["loyer_m2"] = dataframe.loyer / dataframe["surface"]
 print(dataframe.describe())
 dataframe.to_json("data/house/house.json")
 
+# 1 data
+y = dataframe.loyer
+x = dataframe.surface.values.reshape(-1, 1)
 
+# 2 = créer le modèle
+model = lm.LinearRegression()
+
+# 3 = Fit = Apprentissage
+model.fit(x, y)
+
+# 4 Prédiction
+predicted = model.predict(x)
+
+# 5 Display
 plt.scatter(dataframe["surface"], dataframe.loyer, label="Surface / Loyer", color="red")
+plt.plot(dataframe.surface, predicted, color="blue", label="Predicted")
 plt.legend()
 plt.title("Paris")
 plt.show()
