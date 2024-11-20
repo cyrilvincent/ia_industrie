d44ca1259f9407333b0b6b59902454f72f630bcc
Cyril Vincent <contact@cyrilvincent.com>
2024-11-20 14:02:24 +0100

    Seche

--- demo_house.py
+++ demo_house.py
@@ -8,6 +8,7 @@ dataframe["loyer_m2"] = dataframe.loyer / dataframe["surface"]
 print(dataframe.describe())
 dataframe.to_json("data/house/house.json")
 
+
 plt.scatter(dataframe["surface"], dataframe.loyer, label="Surface / Loyer", color="red")
 plt.legend()
 plt.title("Paris")
