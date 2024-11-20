1d9a6d4cf9af75f6eddadc1b4e656ce9e3beb241
Cyril Vincent <contact@cyrilvincent.com>
2024-11-20 13:51:01 +0100

    Seche

---
+++ demo_house.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print(pd.__version__)
+
+dataframe = pd.read_csv("data/house/house.csv")
+dataframe["loyer_m2"] = dataframe.loyer / dataframe["surface"]
+print(dataframe.describe())
+dataframe.to_json("data/house/house.json")
+
+plt.scatter(dataframe["surface"], dataframe.loyer, label="Surface / Loyer", color="red")
+plt.legend()
+plt.title("Paris")
+plt.show()
+
---
+++ demo_python.py
@@ -0,0 +1,11 @@
+x=1
+print(x)
+
+if x == 1:
+    print("Hello")
+
+print("Coucou")
+
+for i in range(10):
+    if i % 2 == 0:
+        print(f"Valeur de i: {i}")
