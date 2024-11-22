0e33d3e4fc60e243df9594917604ba1db16adb47
Cyril Vincent <contact@cyrilvincent.com>
2024-11-22 14:48:46 +0100

    Seche

--- seche_automatisme.py
+++ seche_automatisme.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-dataframe = pd.read_excel("data/seche/Automatisme2024-11-201740.xlsx", skiprows=[1])
+dataframe = pd.read_excel("data/seche/Automatisme2024-11-201740.xlsx", skiprows=[1], na_values="NS")
 dataframe.rename( columns={'Unnamed: 0': 't'}, inplace=True )
 pd.set_option("display.max_columns", None)
 pd.set_option('display.width', None)
 dataframe.set_index("t")
 print(dataframe.head(5))
+print(dataframe.describe())
