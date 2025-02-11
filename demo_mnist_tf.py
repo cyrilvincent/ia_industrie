2b83a1cfb0e66e6dc729b66762550126674da1b3
Cyril Vincent <contact@cyrilvincent.com>
2025-02-11 12:11:07 +0100

    Eras

---
+++ demo_mnist_tf.py
@@ -0,0 +1,61 @@
+import pickle
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+np.random.seed(0)
+
+with np.load("data/mnist/mnist.npz", allow_pickle=True) as f:
+    x_train, y_train = f["x_train"], f["y_train"] # 60000
+    x_test, y_test = f["x_test"], f["y_test"] # 10000
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+x_train = (x_train - 127.5) / 127.5
+x_test = (x_test - 127.5) / 127.5
+
+x_train = x_train.reshape(-1, 28*28)
+x_test = x_test.reshape(-1, 28*28)
+
+y_train = tf.keras.utils.to_categorical(y_train)
+y_test = tf.keras.utils.to_categorical(y_test)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(784, activation="relu", input_shape=(x_train.shape[1],)),
+    tf.keras.layers.Dense(500, activation="relu"),
+    tf.keras.layers.Dense(200, activation="relu"),
+    tf.keras.layers.Dense(10, activation="softmax"),
+  ])
+
+model.compile(loss="categorical_crossentropy", metrics=["accuracy"])
+print(model.summary())
+model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test)
+print(score)
+
+model.save("data/mnist/mnist.h5")
+
+predicted = model.predict(x_test)
+print(predicted)
+
+
+# Gestion des erreurs
+# on récupère les données mal prédites
+predicted = predicted.argmax(axis=1)
+misclass = (y_test.argmax(axis=1) != predicted)
+x_test = x_test.reshape((-1, 28, 28))
+misclass_images = x_test[misclass,:,:]
+misclass_predicted = predicted[misclass]
+
+# on sélectionne un échantillon de ces images
+select = np.random.randint(misclass_images.shape[0], size=12)
+
+# on affiche les images et les prédictions (erronées) associées à ces images
+for index, value in enumerate(select):
+    plt.subplot(3,4,index+1)
+    plt.axis('off')
+    plt.imshow(misclass_images[value],cmap=plt.cm.gray_r,interpolation="nearest")
+    plt.title('Predicted: %i' % misclass_predicted[value])
+
+plt.show()
--- tp_diffraction.py
+++ tp_diffraction.py
@@ -2,6 +2,7 @@ import sklearn.neighbors as nn
 import sklearn.model_selection as ms
 import sklearn.ensemble as rf
 import sklearn.preprocessing as pp
+import sklearn.svm as svm
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
@@ -30,23 +31,24 @@ print(scaler.inverse_transform(xtest))
 #     print(k, score)
 
 # model = nn.KNeighborsClassifier(n_neighbors=11)
-model = rf.RandomForestClassifier()
+# model = rf.RandomForestClassifier()
+model = svm.SVC()
 model.fit(xtrain, ytrain)
 score = model.score(xtest, ytest)
 print(score)
 ypred = model.predict([[11.52,18.75,73.34,409,0.09524,0.05473,0.03036,0.02278,0.192,0.05907,0.3249,0.9591,2.183,23.47,0.008328,0.008722,0.01349,0.00867,0.03218,0.002386,12.84,22.47,81.81,506.2,0.1249,0.0872,0.09076,0.06316,0.3306,0.07036]])
 print(ypred)
 
-plt.bar(x.columns, model.feature_importances_)
-plt.xticks(rotation=45)
-plt.show()
+# plt.bar(x.columns, model.feature_importances_)
+# plt.xticks(rotation=45)
+# plt.show()
 
-from sklearn.tree import export_graphviz
-export_graphviz(model.estimators_[0],
-                out_file="data/diffraction/tree.dot",
-                feature_names=x.columns,
-                class_names=["0", "1"],
-                )
+# from sklearn.tree import export_graphviz
+# export_graphviz(model.estimators_[0],
+#                 out_file="data/diffraction/tree.dot",
+#                 feature_names=x.columns,
+#                 class_names=["0", "1"],
+#                 )
 
-with open("data/diffraction/rf.pickle", "wb") as f:
+with open("data/diffraction/svm.pickle", "wb") as f:
     pickle.dump(model, f)
