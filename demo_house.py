65399f2a5c9458b286a600f3bad73fdf73205a0a
Cyril Vincent <contact@cyrilvincent.com>
2025-12-15 14:26:40 +0100

    Sparks

--- demo_house.py
+++ demo_house.py
@@ -18,35 +18,35 @@ dataframe.to_json("data/house/house.json")
 y = dataframe.loyer
 x = dataframe.surface.values.reshape(-1, 1)
 
-np.random.seed(0)
-xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+# np.random.seed(0)
+# xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
 
 # 2 = créer le modèle
-# model = lm.LinearRegression()
-degre = 1
+model = lm.LinearRegression()
+# degre = 1
 # model = pipe.make_pipeline(pp.PolynomialFeatures(degre), lm.Ridge())
-for k in range(3,12,2):
-    model = nn.KNeighborsClassifier(n_neighbors=k)
-    model.fit(xtrain, ytrain)
-    print(k, model.score(xtest, ytest))
+# for k in range(3,12,2):
+#     model = nn.KNeighborsClassifier(n_neighbors=k)
+#     model.fit(xtrain, ytrain)
+#     print(k, model.score(xtest, ytest))
 
 # 3 = Fit = Apprentissage
 # Un seul interdit, ne JAMAIS s'entrainer sur le xtest
 
-
+model.fit(x, y)
 
 xsample = np.arange(400).reshape(-1, 1)
 # 4 Prédiction
 predicted = model.predict(xsample)
 
-score_train = model.score(xtrain, ytrain)
-score_test = model.score(xtest, ytest)
-print(score_train, score_test)
+# score_train = model.score(xtrain, ytrain)
+# score_test = model.score(xtest, ytest)
+# print(score_train, score_test)
 
 # print(model.coef_, model.intercept_)
 
 # 5 Display
-plt.scatter(dataframe["surface"], dataframe.loyer, label="Surface / Loyer", color="red")
+plt.scatter(dataframe["surface"], dataframe["loyer"], label="Surface / Loyer", color="red")
 plt.plot(xsample, predicted, color="blue", label="Predicted")
 plt.legend()
 plt.title("Paris")
