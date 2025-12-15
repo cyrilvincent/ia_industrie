e5886ee78ca06dbbd7fc5840084259c57b21c298
Cyril Vincent <contact@cyrilvincent.com>
2025-12-15 15:54:42 +0100

    Sparks

--- demo_diffraction.py
+++ demo_diffraction.py
@@ -38,8 +38,8 @@ xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_si
 
 
 # model = lm.LinearRegression()
-model = n.KNeighborsClassifier(n_neighbors=5)
-# model = rf.RandomForestClassifier(max_depth=5)
+# model = n.KNeighborsClassifier(n_neighbors=5)
+model = rf.RandomForestClassifier()
 # model = nn.MLPClassifier(hidden_layer_sizes=(30,10))
 model.fit(xtrain, ytrain)
 score = model.score(xtrain, ytrain)
@@ -49,21 +49,20 @@ print(score)
 ypred = model.predict(xtest)
 
 print(m.classification_report(ytest, ypred))
-
 print(m.confusion_matrix(ytest, ypred))
 
-# print(model.feature_importances_)
-# plt.bar(xorig.columns, model.feature_importances_)
-# plt.xticks(rotation=45)
-# plt.show()
-#
-# from sklearn.tree import export_graphviz
-# export_graphviz(model.estimators_[0],
-#                  out_file='data/diffraction/tree.dot',
-#                  feature_names = xorig.columns,
-#                  class_names = ["0", "1"],
-#                  rounded = True, proportion = False,
-#                  precision = 2, filled = True)
+print(model.feature_importances_)
+plt.bar(xorig.columns, model.feature_importances_)
+plt.xticks(rotation=45)
+plt.show()
+
+from sklearn.tree import export_graphviz
+export_graphviz(model.estimators_[0],
+                 out_file='data/diffraction/tree.dot',
+                 feature_names = xorig.columns,
+                 class_names = ["0", "1"],
+                 rounded = True, proportion = False,
+                 precision = 2, filled = True)
 
 
 
